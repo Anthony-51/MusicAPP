1531426c2fa4e6bc5c5f48abb3507b9fabacbb11
Alex <alex.apd@hotmail.com>
2021-06-01 23:14:12 -0500

    Recycler aplicado

--- app/src/main/java/com/example/reproductor/MainActivity.java
+++ app/src/main/java/com/example/reproductor/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.reproductor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager gridLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerMain);
+        gridLayout = new GridLayoutManager(getApplicationContext(),2);
+        Log.e("context", "" + getApplicationContext());
+        Log.e("context", "" + this);
     }
 }
