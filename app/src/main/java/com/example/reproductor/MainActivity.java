d020b7f5f6abbb6b67a06d2218d0c1580ad92cbd
Alex <alex.apd@hotmail.com>
2021-06-03 01:24:04 -0500

    Navegador superior insertado

--- app/src/main/java/com/example/reproductor/MainActivity.java
+++ app/src/main/java/com/example/reproductor/MainActivity.java
@@ -1,18 +1,36 @@
 package com.example.reproductor;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.reproductor.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
     private ActivityMainBinding binding;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        mToggle = new ActionBarDrawerToggle(this,binding.drawerLayout,R.string.open,R.string.close);
+        binding.drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
